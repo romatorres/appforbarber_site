d4af40887806ad1531f082bfe7dbe6b0075eb4fc
romatorres <romatorres12@gmail.com>
2024-05-13 23:09:59 -0300

    responsive menu adjustments

--- script/script.js
+++ script/script.js
@@ -1,6 +1,20 @@
-function Menu(e){
-    let list = document.querySelector('ul');
+/* function Menu(e) {
+  let list = document.querySelector("ul");
 
-    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[92px]'), list.classList.add('md:top-[110px]'), list.classList.add("opacity-100") ) 
-    : (e.name = "menu", list.classList.remove('top-[92px]'), list.classList.remove('md:top-[110px]'), list.classList.remove("opacity-100") )  
+  e.name === "menu"
+    ? ((e.name = "close"),
+      list.classList.add("top-[92px]"),
+      list.classList.add("md:top-[110px]"),
+      list.classList.add("opacity-100"))
+    : ((e.name = "menu"),
+      list.classList.remove("top-[92px]"),
+      list.classList.remove("md:top-[110px]"),
+      list.classList.remove("opacity-100"));
+} */
+
+function Menu(e) {
+  let list = document.querySelector("ul");
+
+  e.name === "menu" ? "close" : " menu";
+  list.classList.toggle("top-[92px]"), list.classList.toggle("opacity-100");
 }
