da175d8e927e24d321ab33408234a02e2d7c8e92
romatorres <romatorres12@gmail.com>
2024-05-16 21:08:46 -0300

    anchor correction

--- script/script.js
+++ script/script.js
@@ -1,24 +1,3 @@
-/* function Menu(e) {
-  let list = document.querySelector("ul");
-
-  e.name === "menu"
-    ? ((e.name = "close"),
-      list.classList.add("top-[92px]"),
-      list.classList.add("md:top-[110px]"),
-      list.classList.add("opacity-100"))
-    : ((e.name = "menu"),
-      list.classList.remove("top-[92px]"),
-      list.classList.remove("md:top-[110px]"),
-      list.classList.remove("opacity-100"));
-} */
-
-/* function Menu(e) {
-  let list = document.querySelector("ul");
-
-  e.name === "menu" ? (e.name = "close") : (e.name = "menu");
-  list.classList.toggle("top-[92px]"), list.classList.toggle("opacity-100");
-} */
-
 const button1 = document.getElementById("btn_toggle1");
 const button2 = document.getElementById("btn_toggle2");
 const button3 = document.getElementById("btn_toggle3");
@@ -26,20 +5,15 @@ const button3 = document.getElementById("btn_toggle3");
 let list = document.querySelector("ul");
 
 function handleClick() {
-  button1.classList.toggle("origin-center"), button1.classList.toggle("-rotate-45"), button1.classList.toggle("translate-x-[-5px]"), button1.classList.toggle("translate-y-[5px]");
+  button1.classList.toggle("origin-center"),
+    button1.classList.toggle("-rotate-45"),
+    button1.classList.toggle("translate-x-[-5px]"),
+    button1.classList.toggle("translate-y-[5px]");
   button2.classList.toggle("opacity-0"), button2.classList.toggle("w-0");
-  button3.classList.toggle("origin-center"), button3.classList.toggle("rotate-45"), button3.classList.toggle("translate-x-[-5px]"), button3.classList.toggle("translate-y-[-9px]");
+  button3.classList.toggle("origin-center"),
+    button3.classList.toggle("rotate-45"),
+    button3.classList.toggle("translate-x-[-5px]"),
+    button3.classList.toggle("translate-y-[-9px]");
 
   list.classList.toggle("top-[92px]"), list.classList.toggle("opacity-100");
 }
-
-
-
-
-
-
-
-
-
-
-
